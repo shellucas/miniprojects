bef69d7e8575b4442eae5808acd933ed40eabe03
Shelby <shelby.hendrickx@hotmail.com>
2019-01-01 04:43:37 +0100

    fixed bug

--- hangman/hangmanlogic.py
+++ hangman/hangmanlogic.py
@@ -10,51 +10,65 @@ def _init_dictionary():
 
 
 class Hangman:
+    """A classic CLI version of hangman. To play call self.play()"""
 
     def __init__(self):
-        self.max_tries = 5
-        self.current_tries = 0
+        self.attempts = 10
         self.dictionary = _init_dictionary()
         self.word = random.choice(self.dictionary)[:-1]
         self.guessed_letters = []
         self.hidden_word = ''
+        for _ in self.word:
+            self.hidden_word += '_'
 
     def play(self):
-        print(self.dictionary)
-        print(self.word)
+        """The only method needing to be called to play the game."""
         print('=============Welcome to Hangman!=============')
-        while self.word != self.hidden_word or self.current_tries == self.max_tries:
+        while self.attempts != 0 and self.hidden_word != self.word:
             self._print_status()
             self._guess_letter()
-
-        if self.current_tries == self.max_tries:
-            print('Unlucky, You didn\'t guess the word.\n'
-                  'The answer was \'', self.word, '\'.')
         else:
-            print('Congratulations!! You guessed', self.word)
+            print()
+            if self.attempts == 0:
+                print('Unlucky, You didn\'t guess the word.\n'
+                      'The answer was \'' + self.word + '\'.')
+            else:
+                print('Congratulations!! You guessed', self.word)
 
     def _print_status(self):
+        """Prints the current status of the game."""
         print()
-        print('The current word has', len(self.word), 'letters.')
-        print('Guessed letters are: ', self.guessed_letters)
+        print('The word I\'m thinking of has', len(self.word), 'letters.')
+        if self.guessed_letters:
+            print('Guessed letters are: ', self.guessed_letters)
+        print('Word:     ', self.hidden_word, '     Attempts:', self.attempts)
+        print()
+
+    def _guess_letter(self):
+        """Loops until the users inputs a valid letter."""
+        while True:
+            guess = input('Guess > ')
+            if len(guess) != 1:
+                print('Please only enter 1 letter.')
+            elif guess in self.guessed_letters:
+                print('You already guessed ' + guess)
+            else:
+                break
+        self._update_status(guess)
+
+    def _update_status(self, guess):
+        """Updates the status of the game."""
+        self.guessed_letters.append(guess)
+        if guess not in self.word:
+            print('Incorrect!')
+            self.attempts -= 1
+        self._update_hidden_word()
+
+    def _update_hidden_word(self):
+        """Updates the hidden word of the current game."""
         self.hidden_word = ''
         for letter in self.word:
             if letter in self.guessed_letters:
                 self.hidden_word += letter
             else:
                 self.hidden_word += '_'
-        print('Word:     ', self.hidden_word, '     Tries:', self.current_tries)
-        print()
-
-    def _guess_letter(self):
-        while True:
-            guess = input('Guess > ')
-            if len(guess) != 1:
-                print('Please only enter 1 letter')
-            elif guess in self.guessed_letters:
-                print('You already guessed ' + guess)
-            else:
-                break
-        self.guessed_letters.append(guess)
-        if guess not in self.word:
-            self.current_tries += 1
